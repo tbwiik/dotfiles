071b07077c181bcae93017633d54b52d94493978
Torbjørn Wiik <22935970+tbwiik@users.noreply.github.com>
2026-08-12 15:35:37 +0200

    refactor: move nvim setup to dotfiles

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,12 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
