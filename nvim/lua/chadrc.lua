3ef233ff655b81ebc7d4ed4142502a54144cc457
Torbjørn Wiik <22935970+tbwiik@users.noreply.github.com>
2026-08-20 15:25:45 +0200

    fix: syntax error

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+	theme = "material-darker",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
@@ -19,7 +19,8 @@ M.ui = {
 --       tabufline = {
 --          lazyload = false
 --      }
-     vim.o.autoread = true
 }
+
+vim.o.autoread = true
 
 return M
